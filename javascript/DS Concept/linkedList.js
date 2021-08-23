09f16750a87253a0eaef92d1ac1ce8fb7b803650
TrilochanSahoo <71021905+TrilochanSahoo@users.noreply.github.com>
2021-08-23 20:28:00 +0530

    update : adding program

--- javascript/DS Concept/linkedList.js
+++ javascript/DS Concept/linkedList.js
@@ -1,47 +1,47 @@
-// function LinkedList(){
-//     var length = 0
-//     var head = null
+function LinkedList(){
+    var length = 0
+    var head = null
 
-//     var Node = function(element){
-//         this.element = element
-//         this.next = null
-//     }
+    var Node = function(element){
+        this.element = element
+        this.next = null
+    }
 
-//     this.size = function(){
-//         return length
-//     }
+    this.size = function(){
+        return length
+    }
 
-//     this.head = function(){
-//         return head
-//     }
+    this.head = function(){
+        return head
+    }
 
-//     this.add = function(element){
-//         var node = new Node(element)
-//         if(head === null){
-//             head = node
-//         }
-//         else{
-//             currentNode = head
-//             while(currentNode.next){
-//                 currentNode = currentNode.next
-//             }
-//             currentNode.next = node
-//         }
-//         length++
-//     }
+    this.add = function(element){
+        var node = new Node(element)
+        if(head === null){
+            head = node
+        }
+        else{
+            currentNode = head
+            while(currentNode.next){
+                currentNode = currentNode.next
+            }
+            currentNode.next = node
+        }
+        length++
+    }
 
-//     this.remove = function(element){
-//         var currentNode = head
-//         var previousNode
-//         if(currentNode.element === element){
-//             head = currentNode.next
-//         }
-//         else{
-//             while(currentNode.element !== element){
-//                 previousNode = currentNode
-//                 currentNode = currentNode.next
-//             }
-//             previousNode
-//         }
-//     }
-// }
+    this.remove = function(element){
+        var currentNode = head
+        var previousNode
+        if(currentNode.element === element){
+            head = currentNode.next
+        }
+        else{
+            while(currentNode.element !== element){
+                previousNode = currentNode
+                currentNode = currentNode.next
+            }
+            previousNode
+        }
+    }
+}
