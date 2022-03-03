0ff6d4207f1cc2cc7b3385047cbd27c1ee1b3e2c
TrilochanSahoo <trilochansahoo026@gmail.com>
2022-03-03 23:04:45 +0530

    problem solved

---
+++ python/Leetcode Problems/078-Subsets_1.py
@@ -0,0 +1,23 @@
+# Given an integer array nums of unique elements, return all possible subsets (the power set).
+
+# The solution set must not contain duplicate subsets. Return the solution in any order.
+ 
+
+# Example 1:
+
+# Input: nums = [1,2,3]
+# Output: [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+# Example 2:
+
+# Input: nums = [0]
+# Output: [[],[0]]
+
+
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        num = [[]]
+        for i in nums:
+            # print([i])
+            num += [[i]+x for x in num]
+        # print(num)
+        return num
